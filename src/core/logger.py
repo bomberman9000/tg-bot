abe5822eba8336d338a33be1bada398792d1932a
Александр Шаталов <shatalov82@outlook.com>
2026-01-24 21:52:11 +0400

    fix: logger import

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,17 +1,23 @@
 import logging
 import sys
-from src.core.config import get_settings
+from src.core.config import settings
 
-def setup_logger() -> logging.Logger:
-    settings = get_settings()
-    logger = logging.getLogger("bot")
+def setup_logger(name: str = "bot") -> logging.Logger:
+    logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG if settings.debug else logging.INFO)
+    
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter(
+    handler.setLevel(logging.DEBUG if settings.debug else logging.INFO)
+    
+    formatter = logging.Formatter(
         "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
-    ))
-    logger.addHandler(handler)
+    )
+    handler.setFormatter(formatter)
+    
+    if not logger.handlers:
+        logger.addHandler(handler)
+    
     return logger
 
 logger = setup_logger()
